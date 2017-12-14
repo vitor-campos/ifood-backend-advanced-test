2f7e45ed925110ffc53652c51ccf90ce92629c13
Vitor Campos <vitor.campos@gmail.com>
2017-12-14 07:59:44 -0200

    Migrating RestTemplate to Feign

--- src/main/java/com/ifood/backend/advancedtest/AdvancedTestApplication.java
+++ src/main/java/com/ifood/backend/advancedtest/AdvancedTestApplication.java
@@ -2,8 +2,10 @@ package com.ifood.backend.advancedtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AdvancedTestApplication {
 
 	public static void main(String[] args) {
