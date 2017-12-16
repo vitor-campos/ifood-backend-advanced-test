3e1cb8678a09c33b107edfe933f7dcc4d984c6fb
Vitor Campos <vitor.campos@gmail.com>
2017-12-16 19:36:42 -0200

    Equalização do uso do Feign na API do OpenWeather e criação do interceptor para incluir o APPID nas chamadas

---
+++ src/main/java/com/ifood/backend/configuration/OpenWeatherServiceConfiguration.java
@@ -0,0 +1,51 @@
+package com.ifood.backend.configuration;
+
+
+import com.ifood.backend.advancedtest.service.openweather.OpenWeatherApiClient;
+import com.ifood.backend.advancedtest.service.openweather.interceptor.OpenWeatherRequestInterceptor;
+import com.ifood.backend.advancedtest.util.ApiClient;
+import feign.Contract;
+import feign.Logger;
+import feign.Request;
+import feign.RequestInterceptor;
+import feign.slf4j.Slf4jLogger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenWeatherServiceConfiguration {
+
+    @Value("${services.weather.uri}")
+    private String apiUri;
+
+    @Value("${services.weather.timeout}")
+    private int spotifyTimeout;
+
+    @Bean
+    public OpenWeatherApiClient createApi() {
+        return buildBaseApiClient()
+                .setBasePath(apiUri)
+                .buildClient(OpenWeatherApiClient.class);
+    }
+
+    private ApiClient buildBaseApiClient() {
+        ApiClient apiClient = new ApiClient();
+        apiClient.getFeignBuilder().options(new Request.Options(spotifyTimeout, spotifyTimeout));
+        apiClient.getFeignBuilder().logger(new Slf4jLogger());
+        apiClient.getFeignBuilder().logLevel(Logger.Level.FULL);
+        apiClient.getFeignBuilder().requestInterceptor(getRequestInterceptor());
+        return apiClient;
+    }
+
+    @Bean
+    public RequestInterceptor getRequestInterceptor() {
+        return new OpenWeatherRequestInterceptor();
+    }
+
+    @Bean
+    public Contract useFeignAnnotations() {
+        return new Contract.Default();
+    }
+
+}
